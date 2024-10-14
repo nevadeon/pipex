a3205812f02bc982c1151d69ce5f0dc598ebf72b
ndavenne <github@noedavenne.aleeas.com>
2024-10-14 16:21:39 +0200

    initial commit

---
+++ pipex.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 10:16:25 by ndavenne          #+#    #+#             */
+/*   Updated: 2024/10/14 16:18:51 by ndavenne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+#include "libndav.h"
+
+char	**parse_env_paths(char **env)
+{
+	char	**env_paths;
+
+	while (*env != NULL)
+	{
+		if (ft_strncmp(*env, "PATH=", 5) == 0)
+		{
+			*env = *env + 5;
+			break ;
+		}
+		env++;
+	}
+	env_paths = ft_split(*env, ':');
+	return (env_paths);
+}
+
+char	***parse_cmds(int argc, char **argv)
+{
+	char	***cmds;
+	int		i;
+
+	i = 0;
+	while (i + 2 < argc - 1)
+	{
+		cmds[i] = ft_split(argv[i + 2], ' ');
+		i++;
+	}
+	return (cmds);
+}
+
+void	print_string_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i] != NULL)
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+}
+
+void	check_args(int argc, char **argv)
+{
+	if (argc < 5)
+	{
+		printf("not enough arguments\n");
+		exit(1);
+	}
+	if (access(argv[1], R_OK) != 0)
+	{
+		printf("no permission to read infile\n");
+		exit (1);
+	}
+	if (access(argv[4], W_OK) != 0)
+	{
+		printf("no permission to write to outfile\n");
+		exit (1);
+	}
+}
+
+void	exec_cmds(char ***cmds, char **env_paths)
+{
+
+}
+
+int	main(void)
+{
+	int		argc = 5;
+	char	*argv[] = {"a.out", "infile", "cat", "wc -l", "outfile"};
+	char	*envp[] = {"user=davdav", "PATH=/bin:/usr/bin"};
+	char	**env_paths;
+	char	***cmds;
+
+	check_args(argc, argv);
+	env_paths = parse_env_paths(envp);
+	cmds = parse_cmds(argc, argv);
+	exec_cmds(cmds, env_paths);
+	return (0);
+}
---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 10:57:07 by ndavenne          #+#    #+#             */
+/*   Updated: 2024/10/10 13:16:46 by ndavenne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+#endif
