d774c29eedf61c5941da755cff6ef4d97c3529e0
ndavenne <github@noedavenne.aleeas.com>
2024-10-22 17:24:45 +0200

    test version of pipex (do not read this for your sanity)

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 10:16:25 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/10/14 16:41:07 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:23:22 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ char	**parse_env_paths(char **env)
 		}
 		env++;
 	}
+	if (*env == NULL)
+		return (NULL);
 	env_paths = ft_split(*env, ':');
 	return (env_paths);
 }
@@ -33,14 +35,18 @@ char	**parse_env_paths(char **env)
 char	***parse_cmds(int argc, char **argv)
 {
 	char	***cmds;
+	int		nb_cmds;
 	int		i;
 
+	nb_cmds = argc - 3;
+	cmds = (char ***)malloc(sizeof(char **) * (nb_cmds + 1));
 	i = 0;
-	while (i + 2 < argc - 1)
+	while (i < nb_cmds)
 	{
 		cmds[i] = ft_split(argv[i + 2], ' ');
 		i++;
 	}
+	cmds[nb_cmds] = NULL;
 	return (cmds);
 }
 
@@ -75,44 +81,102 @@ void	check_args(int argc, char **argv)
 	}
 }
 
+char	*ft_pathjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	char	*joined_o;
+
+	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 2));
+	if (joined == NULL)
+		return (NULL);
+	joined_o = joined;
+	while (*s1 != '\0')
+		*joined++ = *s1++;
+	*joined++ = '/';
+	while (*s2 != '\0')
+		*joined++ = *s2++;
+	*joined = '\0';
+	return (joined_o);
+}
+
 void	exec_cmds(char ***cmds, char **env_paths, char **envp)
 {
+	char	**env_paths2;
 	int		pipefd[2];
-	pid_t	pid;
+	pid_t	pid1;
+	pid_t	pid2;
 
+	env_paths2 = env_paths;
 	if (pipe(pipefd) == -1)
 	{
 		perror("pipe error");
 		exit(EXIT_FAILURE);
 	}
-	pid = fork();
-	if (pid == -1)
+	int fd = open("infile", O_RDONLY);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	pid1 = fork();
+	if (pid1 == -1)
 	{
 		perror("fork error");
 		exit(EXIT_FAILURE);
 	}
-	if (pid == 0)
+	if (pid1 == 0)
 	{
-		dup2();
-		execve(ft_strjoin("/bin", "/cat"), cmds[0], envp);
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		while (*env_paths != NULL)
+		{
+			execve(ft_pathjoin(*env_paths, cmds[0][0]), cmds[0], envp);
+			env_paths++;
+		}
+		printf("command 1 failure\n");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		wait(NULL);
+		pid2 = fork();
+		if (pid2 == -1)
+		{
+			perror("pipe error");
+			exit(1);
+		}
+		if (pid2 == 0)
+		{
+			close(pipefd[1]);
+			dup2(pipefd[0], STDIN_FILENO);
+			close(pipefd[0]);
+			while (*env_paths2 != NULL)
+			{
+				execve(ft_pathjoin(*env_paths2, cmds[1][0]), cmds[1], envp);
+				env_paths2++;
+			}
+		}
 	}
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
-	int		argc = 5;
-	char	*argv[] = {"a.out", "infile", "cat", "wc -l", "outfile"};
-	char	*envp[] = {"user=davdav", "PATH=/bin:/usr/bin"};
+	argc = 5;
+	char	*argtest[] = {"a.out", "infile", "cat", "wc -l", "outfile"};
 	char	**env_paths;
 	char	***cmds;
 
-	check_args(argc, argv);
+	check_args(argc, argtest);
 	env_paths = parse_env_paths(envp);
-	cmds = parse_cmds(argc, argv);
+	cmds = parse_cmds(argc, argtest);
+
+	//print test
+	// print_string_tab(env_paths);
+	// int i = -1;
+	// while (cmds[++i] != NULL)
+	// {
+	// 	print_string_tab(cmds[i]);
+	// 	printf("-----------\n");
+	// }
+	// print_string_tab(envp);
+
 	exec_cmds(cmds, env_paths, envp);
 	return (0);
 }
