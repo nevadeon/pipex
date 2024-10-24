de541ff228b125b03a9c1c1b8934bc4ecc85b57a
ndavenne <github@noedavenne.aleeas.com>
2024-10-24 16:47:46 +0200

    changed behavior when arena is out of mem

--- ft_arena.c
+++ ft_arena.c
@@ -6,7 +6,7 @@
 /*   By: ndavenne <ndavenne@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:19:51 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/10/24 16:35:31 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/10/24 16:47:06 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,7 @@ void	*ft_arena(ssize_t size)
 		return (NULL);
 	}
 	if (pos + (size_t) size > ARENA_SIZE)
-	{
-		free(ptr);
-		printf("mem error\n");
-		exit(EXIT_FAILURE);
-	}
+		return (NULL);
 	pos += (size_t) size;
 	return (ptr + pos - (size_t) size);
 }
