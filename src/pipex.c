65d9b74be0fd736e716a613ddf1e8975add6b59a
ndavenne <github@noedavenne.aleeas.com>
2024-11-06 21:52:07 +0100

    removed commented code and added exec_cmds

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ndavenne <github@noedavenne.aleaas.coms    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 10:16:25 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/10/28 11:17:23 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/11/06 21:50:16 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,171 +48,67 @@ char	*ft_pathjoin(char const *s1, char const *s2)
 	return (joined_o);
 }
 
-// void	exec_cmds(char ***cmds, char **env_paths, char **envp)
-// {
-// 	char	**env_paths2;
-// 	int		pipefd[2];
-// 	pid_t	pid;
-// 	int fd;
+void	exec_last_cmd(t_pipex *p, char **envp, int pipe_fd[2])
+{
+	char	*path;
+	pid_t	pid;
+	int		i;
 
-// 	env_paths2 = env_paths;
-// 	if (pipe(pipefd) == -1)
-// 	{
-// 		perror("pipe error");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	pid = fork();
-// 	if (pid == -1)
-// 	{
-// 		perror("fork error");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	if (pid == 0)
-// 	{
-// 		close(pipefd[0]);
-// 		dup2(pipefd[1], STDOUT_FILENO);
-// 		close(pipefd[1]);
-// 		fd = open("infile", O_RDONLY);
-// 		dup2(fd, STDIN_FILENO);
-// 		close(fd);
-// 		fd = 0;
-// 		while (*env_paths != NULL)
-// 		{
-// 			execve(ft_pathjoin(*env_paths, cmds[0][0]), cmds[0], envp);
-// 			env_paths++;
-// 		}
-// 		printf("command 1 failure\n");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	else
-// 	{
-// 		pid = fork();
-// 		if (pid == -1)
-// 		{
-// 			perror("emma error");
-// 			exit(1);
-// 		}
-// 		if (pid == 0)
-// 		{
-// 			close(pipefd[1]);
-// 			dup2(pipefd[0], STDIN_FILENO);
-// 			close(pipefd[0]);
-// 			fd = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-// 			dup2(fd, STDOUT_FILENO);
-// 			close(fd);
-// 			while (*env_paths2 != NULL)
-// 			{
-// 				execve(ft_pathjoin(*env_paths2, cmds[1][0]), cmds[1], envp);
-// 				env_paths2++;
-// 			}
-// 		}
-// 	}
-// }
+	pid = fork();
+	if (pid == -1)
+		error();
+	if (pid == 0)
+	{
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[0]);
+		dup2(p->out_fd, STDOUT_FILENO);
+		close(p->out_fd);
+		i = 0;
+		while (p->env_paths[i] != NULL)
+		{
+			path = ft_pathjoin(p->env_paths[i], p->cmds[p->cmd_count - 1][0]);
+			execve(path, p->cmds[p->cmd_count - 1], envp);
+			i++;
+		}
+	}
+}
 
-// int	*exec_first_cmd(t_pipex *p, char **envp)
-// {
-// 	pid_t	pid;
-// 	char	*path;
-// 	int		pipe_fd[2];
-// 	int		i;
+void	exec_first_cmd(t_pipex *p, char **envp, int pipe_fd[2])
+{
+	char	*path;
+	pid_t	pid;
+	int		i;
 
-// 	if (pipe(pipe_fd) == -1)
-// 		error();
-// 	pid = fork();
-// 	if (pid == -1)
-// 		error();
-// 	if (pid == 0)
-// 	{
-// 		close(pipe_fd[1]);
-// 		dup2(p->in_fd, STDIN_FILENO);
-// 		close(p->in_fd);
-// 		i = 0;
-// 		while (p->env_paths[i] != NULL)
-// 		{
-// 			path = ft_pathjoin(p->env_paths[i], p->cmds[0][0]);
-// 			execve(path, p->cmds[0], envp);
-// 			i++;
-// 		}
-// 	}
-// }
+	pid = fork();
+	if (pid == -1)
+		error();
+	if (pid == 0)
+	{
+		close(pipe_fd[0]);
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[1]);
+		dup2(p->in_fd, STDIN_FILENO);
+		close(p->in_fd);
+		i = 0;
+		while (p->env_paths[i] != NULL)
+		{
+			path = ft_pathjoin(p->env_paths[i], p->cmds[0][0]);
+			execve(path, p->cmds[0], envp);
+			i++;
+		}
+	}
+}
 
-// void	exec_last_cmd(t_pipex *p, char **envp)
-// {
-// 	pid_t	pid;
-// 	char	*path;
-// 	int		pipe_fd[2];
-// 	int		i;
+void	exec_cmds(t_pipex *p, char **envp)
+{
+	int pipe_fd[2];
 
-// 	pid = fork();
-// 	if (pid == -1)
-// 		error();
-// 	if (pid == 0)
-// 	{
-// 		close(pipe_fd[0]);
-// 		dup2(p->out_fd, STDOUT_FILENO);
-// 		close(p->out_fd);
-// 		i = 0;
-// 		while (p->env_paths[i] != NULL)
-// 		{
-// 			path = ft_pathjoin(p->env_paths[i], p->cmds[0][0]);
-// 			execve(path, p->cmds[0], envp);
-// 			i++;
-// 		}
-// 	}
-// }
-
-// void	exec_cmds(t_pipex *p, char **envp)
-// {
-// 	pid_t	pid;
-// 	char	*path;
-// 	int		pipe_fd[2];
-// 	int		i;
-// 	int		j;
-
-// 	if (pipe(pipe_fd) == -1)
-// 		error();
-// 	pid = fork();
-// 	if (pid == -1)
-// 		error();
-// 	else if (pid == 0)
-// 	{
-// 		close(pipe_fd[0]);
-// 		dup2(pipe_fd[1], STDOUT_FILENO);
-// 		close(pipe_fd[1]);
-// 		dup2(p->in_fd, STDIN_FILENO);
-// 		close(p->in_fd);
-// 		i = 0;
-// 		while(p->env_paths[i] != NULL)
-// 		{
-// 			path = ft_pathjoin(p->env_paths[i], p->cmds[0][0]);
-// 			execve(path, p->cmds[0], envp);
-// 			i++;
-// 		}
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	else
-// 	{
-// 		wait(pid);
-// 		j = 0;
-// 		while (j < p->cmd_count)
-// 		{
-// 			pid = fork();
-// 			if (pid == -1)
-// 			error();
-// 				else if (pid == 0)
-// 				{
-// 					dup2(pipe_fd[0], STDIN_FILENO);
-// 					close(pipe_fd[0]);
-// 					dup2(pipe_fd[1], STDOUT_FILENO);
-// 					close(pipe_fd[1]);
-// 				}
-// 			j++;
-// 		}
-
-// 		wait(pid);
-
-// 	}
-// }
+	if (pipe(pipe_fd) == -1)
+		error();
+	exec_last_cmd(p, envp, pipe_fd);
+	exec_first_cmd(p, envp, pipe_fd);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -223,10 +119,7 @@ int	main(int argc, char **argv, char **envp)
 	p.env_paths = parse_env_paths(envp);
 	p.cmds = parse_cmds(argc, argv);
 	p.cmd_count = argc - 3;
-	// exec_cmds(&p, envp);
-	return (0);
-}
-
+	exec_cmds(&p, envp);
 	// print_string_tab(p.env_paths);
 	// int i = -1;
 	// while (p.cmds[++i] != NULL)
@@ -234,4 +127,7 @@ int	main(int argc, char **argv, char **envp)
 	// 	print_string_tab(p.cmds[i]);
 	// 	printf("-----------\n");
 	// }
-	// print_string_tab(envp);
+	// printf("%s\n", p.cmds[p.cmd_count - 1][0]);
+	return (0);
+}
+
